4a90d921bf9497403e4a4c5226dcea6ab2dcb7d4
Evans Musomi <evans.musomi@gmail.com>
2018-04-16 05:05:59 +0300

    initializes project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Push Server
+
+A JavaScript web push notification server for a Progressive Web App.
---
+++ client/main.js
@@ -0,0 +1,10 @@
+// Update UI with subscription status
+const setSubscribedStatus = state => {
+  if (state) {
+    document.getElementById("subscribe").className = "hidden";
+    document.getElementById("unsubscribe").className = "";
+  } else {
+    document.getElementById("subscribe").className = "";
+    document.getElementById("unsubscribe").className = "hidden";
+  }
+};
