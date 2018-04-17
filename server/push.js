89bb00a924bcd5e7b91efb9d4effd99cf3b91098
Evans Musomi <evans.musomi@gmail.com>
2018-04-17 08:34:10 +0300

    persists subscriptions on server using node-storage

--- server/push.js
+++ server/push.js
@@ -1,18 +1,23 @@
 // Modules
 const webpush = require("web-push");
 const urlsafeBase64 = require("urlsafe-base64");
+const Storage = require("node-storage");
 
 // Vapid keys
 const vapid = require("./vapid.json");
 
 // Subscriptions
-let subscriptions = [];
+const store = new Storage(`${__dirname}/db`);
+let subscriptions = store.get("subscriptions") || [];
 
 // Create url safe vapid public key
 module.exports.getKey = () => urlsafeBase64.decode(vapid.publicKey);
 
 // Add subscriptions to array
 module.exports.addSubscription = subscription => {
+  // Add to subscriptions array
   subscriptions.push(subscription);
-  console.log(subscriptions);
+
+  // Persist subscriptions
+  store.put("subscriptions", subscriptions);
 };
